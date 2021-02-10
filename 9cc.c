26377bc607483df4c9c2d23a2e75fa0b13020b30
shin52 <b1018088@fun.ac.jp>
2021-02-11 01:09:35 +0900

    step2

---
+++ 9cc.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char **argv){
+	if(argc != 2){
+		fprintf(stderr, "引数の個数が正しくありません\n");
+		return 1;
+	}
+
+	char *p = argv[1];
+
+	printf(".intel_syntax noprefix\n");
+	printf(".globl main\n");
+	printf("main:\n");
+	printf(" mov rax, %ld\n", strtol(p, &p, 10));
+
+	while(*p){
+		if(*p == '+'){
+			p++;
+			printf(" add rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		if(*p == '-'){
+			p++;
+			printf(" sub rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+		return 1;
+	}
+
+	printf(" ret\n");
+	return 0;
+}
+			
---
+++ tmp.s
@@ -0,0 +1,7 @@
+.intel_syntax noprefix
+.globl main
+main:
+ mov rax, 5
+ add rax, 20
+ sub rax, 4 
+ ret
